b3c943d499a566602b829e69e93c950971ec01f5
Nathan G. Santos <nathangar01@gmail.com>
2026-04-26 21:11:24 -0300

    feat(tray): add bitcoin icon to system tray

--- src/main/tray.js
+++ src/main/tray.js
@@ -28,7 +28,8 @@ function formatMayer(value) {
 
 class TrayController {
   constructor({ onQuit, onTogglePanel, onToggleWidget }) {
-    const icon = nativeImage.createEmpty();
+    const iconPath = path.join(__dirname, '../../src/assets/bitcoin.png');
+    const icon = nativeImage.createFromPath(iconPath).resize({ width: 16, height: 16 });
     this._tray = new Tray(icon);
     this._onTogglePanel = onTogglePanel;
     this._onQuit = onQuit;
